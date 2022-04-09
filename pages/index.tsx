df7052eb2d19ec37cdab5925a0a271af0294c889
prlina01 <36077702+prlina01@users.noreply.github.com>
2022-04-09 12:19:43 +0200

    Start frontend design

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,85 +1,29 @@
 import type { NextPage } from 'next'
-import {useWeb3React} from "@web3-react/core";
-import {InjectedConnector} from '@web3-react/injected-connector'
-import GreeterContract from "../artifacts/contracts/Greeter.sol/Greeter.json"
 import {ethers} from "ethers";
-import {Greeter, Greeter__factory} from "../typechain";
 import { useEffect, useState} from "react";
 import {useForm} from "react-hook-form";
 import Link from 'next/link'
+import Web3Modal from 'web3modal'
+import { Input, Container, Row, Col, Spacer, Card, Text, Button, Modal, useModal, Loading, Radio, Grid } from "@nextui-org/react";
 
 
 
-// @ts-ignore
-const injected = new InjectedConnector()
 const Home: NextPage = () => {
-  const {account, deactivate, activate, active, library: provider} = useWeb3React()
-  const [greetingMsg, setGreetingMsg] = useState<string>("")
-  const [hasMetamask, setHasMetamask] = useState<boolean>(false)
 
+  const handleSwap = async () => {
+    const web3Modal = new Web3Modal()
+    const connection = await web3Modal.connect()
+    const provider = new ethers.providers.Web3Provider(connection)
 
-  async function connect() {
-    try {
-      await activate(injected)
-      localStorage.setItem('isWalletConnected', 'true')
-    } catch(e) {
-      console.log(e)
-    }
-  }
-
-  async function disconnect() {
-    try {
-      await deactivate()
-      localStorage.setItem('isWalletConnected', 'false')
-    } catch (e) {
-      console.log(e)
-    }
-  }
-
-  useEffect(() => {
-    const connectWalletOnPageLoad = async() => {
-      if(localStorage?.getItem('isWalletConnected') === 'true') {
-        try {
-          await activate(injected)
-          localStorage.setItem('isWalletConnected', 'true')
-        } catch (e) {
-          console.log(e)
-        }
-      }
-    }
-    if(hasMetamask) connectWalletOnPageLoad()
-    // @ts-ignore
-    else if (typeof window.ethereum !== "undefined") setHasMetamask(true);
-
-  }, [])
-
-  function _getContract() {
     const signer = provider.getSigner()
 
-
-    const contractAddress = "0x745f3C80e6c63b21b633bFc56ecB129826937Ca0"
-    const abi = GreeterContract.abi
-    const contract = Greeter__factory.connect(contractAddress, signer)
-    return contract
   }
-
-
-  async function execute(entered_greet: { [greet: string]: string; }) {
-    if(active) {
-      const greet = entered_greet['greetInputField']
-      const contract = _getContract()
-      // const contract = new ethers.Contract(contractAddress, abi, signer)
-      try {
-        await contract.setGreeting(greet)
-        setGreetingMsg(greet)
-      } catch (e) {
-        console.log(e)
-      }
-
-    }
-  }
+  const { setVisible, bindings } = useModal();
+  const [firstToken, setFirstToken] = useState<string>()
+  const [secondToken, setSecondToken] = useState<string>();
 
   const {reset, register, handleSubmit, formState} = useForm()
+
 
   useEffect(() => {
     reset({
@@ -87,53 +31,104 @@ const Home: NextPage = () => {
     })
   }, [reset, formState.isSubmitSuccessful])
 
-  useEffect(() => {
-    const initialGreeting = async () => {
-      const contract = _getContract()
-      const msg = await contract.greet()
-      setGreetingMsg(msg)
-    }
-    active && initialGreeting()
-  }, [active])
+  const showButton = () => {
+    if(!firstToken) {return (<Button auto shadow color="gradient" onClick={() => setVisible(true)} >ETH ∨</Button>)}
+    else if (firstToken === "ETH") {return (<Button size={'xs'}></Button>)}
+  }
 
+  // @ts-ignore
+  // @ts-ignore
+  // @ts-ignore
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      {hasMetamask ? (
-        active ? (
-          <>
-            <span>Connected with <b>{account}</b></span>
-            <span>Greeter: <b>{greetingMsg}</b></span>
-            <form onSubmit={handleSubmit(execute)}>
-              <label>Change greeting:</label>
-              <input {...register("greetInputField", {required: true})} className="border-2 m-2" />
-              <input type="submit" value="submit" className="py-2 px-2 rounded-2xl mt-20 mb-4 text-lg font-bold text-white  bg-blue-600 hover:bg-blue-800"/>
-            </form>
-            <Link href="state">
-              <button className="py-2 px-2 rounded-2xl mt-20 mb-4 text-lg font-bold text-white  bg-blue-600 hover:bg-blue-800">
-                Go to state page[metamask account stays logged in when changing pages]
-              </button>
-            </Link>
+      <div>
+        <Spacer y={10} />
+        <Container xs  >
+          <Card css={{bgColor: "$blue900"}}>
+            <Text h1 size={60} css={{
+              textGradient: "45deg, $blue500 -20%, $pink500 50%",
+              textAlign: "center"
+            }}>
+              Swap
+            </Text>
+            <Grid.Container gap={2} justify="center">
+              <Grid  lg={9} xs={12}>
+                <Input
+                    fullWidth
+                    rounded
+                    bordered
+                    placeholder="0.00"
+                    color="primary"
+                    size="xl"
+                    aria-label="input1"
+                />
+              </Grid>
+              <Grid  lg={3} xs={12} justify='center'>
+                {showButton()}
+              </Grid>
+              <Grid lg={9} justify='center' xs={12}>
+                <button className="text-xs"><Text color='primary' size={50} >&#8623;</Text></button>
+              </Grid>
+              <Grid lg={3} xs={0}/>
+              <Grid lg={9} xs={12}>
+                <Input
+                    fullWidth
+                    rounded
+                    bordered
+                    placeholder="0.00"
+                    color="primary"
+                    size="xl"
+                    aria-label="input2"
+                />
+              </Grid>
+              <Grid lg={3} xs={12} justify='center'>
+                {showButton()}
+              </Grid>
+              <Spacer y={2} />
+              <Grid lg={12} xs={12} justify='center'>
+                <Button auto color='gradient'>Start</Button>
+              </Grid>
+              {/*<Grid lg={3} xs={0} />*/}
+            </Grid.Container>
 
-            <b>Rinkeby testnet</b>
-            <button onClick={() => disconnect()} className="py-2 px-2 rounded-2xl mt-20 mb-4 text-lg font-bold text-white bg-blue-600 hover:bg-blue-800">
-              Log out from account
-            </button>
-          </>
-      ) : (
-          <button onClick={() => connect() } className="py-2 px-2 rounded-2xl mt-20 mb-4 text-lg font-bold text-white  bg-blue-600 hover:bg-blue-800">
-            Connect
-          </button>
-      )): (
-          <>
-            <h1>You don't have metamask installed in your browser</h1>
-            <button className="py-2 px-2 rounded-2xl mt-20 mb-4 text-lg font-bold text-white bg-blue-600 hover:bg-blue-800">
-              <a href="https://metamask.io/download/">Install</a>
-            </button>
-          </>
-      )
-      }
-    </div>
+          </Card>
+          <Modal
+              width="300px"
+              aria-labelledby="modal-title"
+              aria-describedby="modal-description"
+              {...bindings}
+              blur
+              preventClose
+              // @ts-ignore
+              css={{bgColor: 'black'}}
+          >
+
+
+            <Modal.Header css={{cursor: 'default'}}>
+              <Text id="modal-title" size={18}>
+                Choose a currency
+              </Text>
+            </Modal.Header>
+            <Modal.Body css={{cursor: 'default'}}>
+              <Text id="modal-description">
+              </Text>
+                <Button.Group size="xl" vertical color="gradient" flat>
+                  <Button onClick={() => setVisible(false)}>ETH</Button>
+                  <Button onClick={() => setVisible(false)}>Drugi</Button>
+                  <Button onClick={() => setVisible(false)}> Treci</Button>
+                </Button.Group>
+
+            </Modal.Body>
+            <Modal.Footer css={{cursor: 'default'}}>
+              {/*<Button auto color="error" >*/}
+              {/*  Ok*/}
+              {/*</Button>*/}
+            </Modal.Footer>
+          </Modal>
+
+        </Container>
+      </div>
   )
+
 }
 
 export default Home
